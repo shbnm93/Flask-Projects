236db7175728135b2325656ddfb09580552d8d97
shbnm93 <shabnamhamidi1993@gmail.com>
2023-11-16 23:44:49 +0300

    flask templates

--- main4.py
+++ main4.py
@@ -21,5 +21,11 @@ def thank_you():
     return render_template('thankyou.html', first=first, last=last)
 
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
