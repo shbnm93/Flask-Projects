0740e714fefaef198f842213074d264fb68361db
shbnm93 <shabnamhamidi1993@gmail.com>
2023-11-22 18:33:06 +0300

    project 1 migrate is done

--- flask project 1/basic.py
+++ flask project 1/basic.py
@@ -24,14 +24,16 @@ class Puppy(db.Model):
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
     breed = db.Column(db.Text)
+    color = db.Column(db.Text)
 
 
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed, color):
         self.name = name
         self.age = age
         self.breed = breed
+        self.color = color
 
     def __repr__(self):
-        return f"Puppy {self.name} is {self.age} year/s old"
+        return f"Puppy {self.name} is {self.age} year/s old and the Color: {self.color}"
 
