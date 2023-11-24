1d791316312161d7c34b57f3b18ae1e0f84c3c71
shbnm93 <shabnamhamidi1993@gmail.com>
2023-11-24 17:55:43 +0300

    migration of project is done

--- flask project 3/forms.py
+++ flask project 3/forms.py
@@ -5,10 +5,10 @@ from wtforms import StringField, IntegerField, SubmitField
 
 class AddForm(FlaskForm):
 
-    name = StringField('Name of Puppy:')
+    name = StringField('Name of Puppy: ')
     submit = SubmitField('Add Puppy')
 
 class DelForm(FlaskForm):
 
-    id = IntegerField('Id Number of Puppy to Remove:')
+    id = IntegerField('Id Number of Puppy to Remove: ')
     submit = SubmitField('Remove Puppy')
