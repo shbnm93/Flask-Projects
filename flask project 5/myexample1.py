53bdf3a4cc4d8c44814df851a647f37ce042df52
shbnm93 <shabnamhamidi1993@gmail.com>
2023-11-26 19:46:15 +0300

    user authentication with password

---
+++ flask project 5/myexample1.py
@@ -0,0 +1,15 @@
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
+
+
+password = 'supersecretpassword'
+
+
+hashed_password = bcrypt.generate_password_hash(password= password)
+
+# print(hashed_password)
+
+
+check = bcrypt.check_password_hash(hashed_password, 'supersecretpassword')
+print(check)
