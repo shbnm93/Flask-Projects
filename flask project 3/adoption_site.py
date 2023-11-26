a34b9976682010765f3c37d9be47d732677fd031
shbnm93 <shabnamhamidi1993@gmail.com>
2023-11-26 12:21:08 +0300

    database project adoption site

--- flask project 3/adoption_site.py
+++ flask project 3/adoption_site.py
@@ -1,5 +1,5 @@
 import os
-from forms import  AddForm , DelForm
+from forms import  AddForm , DelForm, AddOwnerForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -22,12 +22,32 @@ class Puppy(db.Model):
     __tablename__ = 'puppies'
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.Text)
+    owner = db.relationship('Owner', backref='puppy', uselist=False)
 
     def __init__(self,name):
         self.name = name
 
     def __repr__(self):
-        return f"Puppy name: {self.name}"
+        if self.owner:
+            return f"Puppy name is {self.name} and owner is {self.owner.name}"
+        else:
+            return f"Puppy Name: {self.name} and no owner assigned yet!"
+    
+
+class Owner(db.Model):
+
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text)
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
+
+    def __init__(self, name, puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id
+
+    def __repr__(self):
+        return f"Owner Name: {self.name}"
 
 @app.route('/')
 def index():
@@ -69,6 +89,22 @@ def del_pup():
         return redirect(url_for('list_pup'))
     return render_template('delete.html',form=form)
 
+
+@app.route('/add_owner', methods=['GET', 'POST'])
+def add_owner():
+    form = AddOwnerForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        pup_id = form.pup_id.data
+
+        new_owner = Owner(name, pup_id)
+        db.session.add(new_owner)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+
+    return render_template('add_owner.html',form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
