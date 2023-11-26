ff865a3bd1af09bb22848990a0d3d17f8298bd43
shbnm93 <shabnamhamidi1993@gmail.com>
2023-11-26 22:51:22 +0300

    flask login

---
+++ flask project 6/myproject/forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email() ])
+    password = PasswordField('Password', validators=[DataRequired(())])
+    submit = SubmitField("Log in")
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords must match!')])
+    pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Register!')
+
+
+    def validate_email(self, email):
+        if User.query.filter_by(email=self.email.data).first():
+            raise ValidationError('Email has been registered')
+    def validate_username(self, username):
+        if User.query.filter_by(username=self.username.data).first():
+            raise ValidationError('Username has been registered')
+        
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            if User.query.filter_by(email=email.data).first():
+                raise ValidationError('Email has been registered')
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            if User.query.filter_by(username=username.data).first():
+                raise ValidationError('Username has been registered')
