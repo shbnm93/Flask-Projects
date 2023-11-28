c347070ad2134a565d96d776690622fb60b6c8ff
shbnm93 <shabnamhamidi1993@gmail.com>
2023-11-28 11:28:15 +0300

    error pages

--- flask project 8/companyblog/__init__.py
+++ flask project 8/companyblog/__init__.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 
 from companyblog.core.views import core
+from companyblog.error_pages.handlers import error_pages
+
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
